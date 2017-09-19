39c0355cbea848d9b88273f4ae0d7978b99a18dc
Andreas Rumpf <rumpf_a@web.de>
2017-09-19 10:06:54 +0200

    disable automatic ormin_importer building because the caching does not work yet

--- ormin.nim
+++ ormin.nim
@@ -9,8 +9,8 @@ template importModel*(backend: DbBackend; path, filename: string) {.dirty.} =
   ## imports a model from an SQL file.
   bind fileExists, addFileExt, staticExec, ExeExt, `/`
   static:
-    when not fileExists(addFileExt("tools/ormin_importer", ExeExt)):
-      echo staticExec("nim c tools/ormin_importer", "", "tools/ormin_importer.nim")
+    #when not fileExists(addFileExt("tools/ormin_importer", ExeExt)):
+    #  echo staticExec("nim c tools/ormin_importer", "", "tools/ormin_importer.nim")
     echo staticExec("tools/ormin_importer " & (path / filename) & ".sql")
 
   const dbBackend = backend

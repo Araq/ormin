792310b0780ff929b90590d41e122f5909e80d83
huaxk <huaxk@163.com>
2020-01-08 10:50:46 +0800

    change pg password

--- tests/tpostgres.nim
+++ tests/tpostgres.nim
@@ -3,12 +3,21 @@ import db_postgres
 
 suite "test postgres":
   setup:
-    let db = open("localhost", "postgres", "postgress", "postgres")
+    let db = open("localhost", "test", "test", "test")
   
   test "create table":
     db.exec(sql("""CREATE TABLE myTable (
                  id integer,
                  name varchar(50) not null)"""))
+
+  test "insert data":
+    db.exec(sql"INSERT INTO myTable (id, name) VALUES (0, ?)",
+        "Dominik")
+
+  test "query data":
+    let row = db.getRow(sql"select * from myTable")
+    echo row
+    assert row == ["0", "Dominik"]
 
   teardown:
     db.close()

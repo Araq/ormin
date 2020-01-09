846382db5aee1468b9ceca671b21d05a0c01cbba
huaxk <huaxk@163.com>
2020-01-09 16:04:56 +0800

    test pg crud but delete has problem

--- tests/tpostgres.nim
+++ tests/tpostgres.nim
@@ -1,5 +1,4 @@
-import unittest, strutils
-from db_postgres import exec
+import unittest, sequtils, strformat
 import ../ormin
 
 importModel(DbBackend.postgre, "model_postgres")
@@ -8,27 +7,43 @@ suite "test postgres":
   let db {.global.} = open("localhost", "test", "test", "test")
 
   test "insert data":
-    query:
-      insert users(id=1, name="john3", password="xxxx", lastOnline = !!"CURRENT_TIMESTAMP")
+    for i in 1..3:
+      let name = fmt"john{i}"
+      let password = fmt"pass{i}"
+      query:
+        insert users(id = ?i, name = ?name, password = ?password, lastOnline = !!"CURRENT_TIMESTAMP")
 
   test "query data":
     let users = query:
-      select users(id, name)
-    assert users == [(id: 1, name: "john3")]
+      select users(id, name, password)
+    assert users == [1, 2, 3].mapIt((id: it, name: fmt"john{it}", password: fmt"pass{it}"))
 
-  # test "create table":
-  #   db.exec(sql("""CREATE TABLE myTable (
-  #                id integer,
-  #                name varchar(50) not null)"""))
+  test "query count":
+    let count = query:
+      select users(count(_))
+      limit 1
+    assert count == 3
 
-  # test "insert data":
-  #   db.exec(sql"INSERT INTO myTable (id, name) VALUES (0, ?)",
-  #       "Dominik")
+  test "update data":
+    let updatedstr = "updated"
+    let id = 1
+    query:
+      update users(name = name || ?updatedstr)
+      where id == 1
+    let updatedname = query:
+      select users(name)
+      where id == ?id
+      limit 1
+    assert updatedname == fmt"john{id}{updatedstr}"
 
-  # test "query data":
-  #   let row = db.getRow(sql"select * from myTable")
-  #   assert row == ["0", "Dominik"]      
+  # test "delete data":
+  #   let id = 3
+  #   query:
+  #     delete users
+  #     where id == ?id
+  #   let user = query:
+  #     select users(_)
+  #     where id == ?id
+  #   assert user == []
 
-  for name in tableNames:
-    db.exec(sql("drop table " & name & " cascade"))
   db.close()
